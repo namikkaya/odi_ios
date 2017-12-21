07ab2269b112390ff974cf9042e5992212e979b8
bilal <bilaldogan325@gmail.com>
2017-12-21 05:01:38 +0300

    sdaf

--- Odi/Service Structure/Static Connection/PostConnection.swift
+++ Odi/Service Structure/Static Connection/PostConnection.swift
@@ -51,4 +51,40 @@ class PostConnection
             
             }.resume()
     }
+    func PostConnectionImage(serviceUrl: String)
+    {
+        
+        let urlString : String = serviceUrl
+        
+        let url : NSURL = NSURL(string: urlString)!
+        let theRequest = NSMutableURLRequest(url: url as URL)
+        theRequest.addValue("text/xml; charset=utf-8", forHTTPHeaderField: "Content-Type")
+        theRequest.httpMethod = "POST"
+        
+        
+        
+        URLSession.shared.dataTask(with: theRequest as URLRequest) { (data, response, error) in
+            print("Started Connection..")
+            guard let data = data, error == nil else {                                                 // check for fundamental networking error
+                print("error=\(String(describing: error))")
+                self.delegate?.getError(errMessage: "servisResponse")
+                return
+            }
+            
+            if let httpStatus = response as? HTTPURLResponse, httpStatus.statusCode != 200 {           // check for http errors
+                print("statusCode should be 200, but is \(httpStatus.statusCode)")
+                print("response = \(String(describing: response))")
+                self.delegate?.getError(errMessage: "servisResponse")
+                return
+            }
+            
+            let newXML = SWXMLHash.parse(data)
+            if  self.delegate != nil {
+                DispatchQueue.main.async {
+                    self.delegate?.getJson(xmlData: newXML)
+                }
+            }
+            
+            }.resume()
+    }
 }
